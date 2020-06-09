db80bd09ee40a5fb612c15f33d76cf62c9fac091
Szymon444 <59486720+Szymon444@users.noreply.github.com>
2020-06-09 08:33:34 +0200

    nowe

--- sandbox/src/test/java/pl/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/SquareTests.java
@@ -5,9 +5,9 @@ import org.testng.annotations.Test;
 public class SquareTests {
 
     @Test
-    public void testArea(){
+    public void testArea() {
         Square s = new Square(5);
-        assert  s.area() == 25;
+        assert s.area() == 25;
 
     }
 }
