f1dcf8647284697863a7b6a9b6607c22302b9b93
Szymon444 <59486720+Szymon444@users.noreply.github.com>
2020-06-15 11:23:35 +0200

    lek2 zad5 cz2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/ContactCreationTests.java
@@ -10,33 +10,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import ru.stqa.pft.adressbook.tests.TestBase;
 
 import java.util.*;
 
-public class ContactCreationTests {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-    driver = new FirefoxDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-    driver.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
+public class ContactCreationTests extends TestBase {
 
-  private void login(String user, String password) {
-    driver.manage().window().setSize(new Dimension(1158, 729));
-    driver.findElement(By.name("user")).sendKeys(user);
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-  }
-
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
   @Test
   public void contactrCreationTests() {
 
@@ -45,27 +24,4 @@ public class ContactCreationTests {
     submitNewContact();
   }
 
-  private void submitNewContact() {
-    driver.findElement(By.cssSelector("input:nth-child(87)")).click();
-  }
-
-  private void fillNewContact(ContactData contactData) {
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).sendKeys(contactData.getName());
-    driver.findElement(By.name("middlename")).sendKeys(contactData.getSecondName());
-    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
-    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    driver.findElement(By.name("address")).sendKeys(contactData.getAdress());
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).sendKeys(contactData.getMail());
-    driver.findElement(By.name("notes")).click();
-    driver.findElement(By.name("notes")).sendKeys(contactData.getNotes());
-  }
-
-  private void initNewContact() {
-    driver.findElement(By.linkText("nowy wpis")).click();
-  }
 }
