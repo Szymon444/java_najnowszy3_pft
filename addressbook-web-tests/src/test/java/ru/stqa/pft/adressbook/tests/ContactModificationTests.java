e0c3c124f24c6b765e437860531b17d8319cc54a
Szymon444 <59486720+Szymon444@users.noreply.github.com>
2020-06-25 18:34:03 +0200

    Lek4.Zad10.cz1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.adressbook.model.ContactData;
 
@@ -7,7 +8,10 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void contactModificationTests() {
+
+
     app.getNavigationHelper().goToHomePage();
+    int before = app.getContactHelper().getContactCount();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Bran", "Marian", "Kowalski", "Dominator", "Opel", "Kościuszki 220", "512-22-222", "jan@po.pl", "Będzie dobrze!", "test1"), true);
       app.getNavigationHelper().goToHomePage();
@@ -16,6 +20,9 @@ public class ContactModificationTests extends TestBase {
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillNewContact(new ContactData("Bran", "Marian", "Kowalski", "Dominator", "Opel", "Kościuszki 220", "512-22-222", "jan@po.pl", "Będzie dobrze!", null), false);
     app.getContactHelper().updateContactModification();
+    app.getNavigationHelper().goToHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before);
   }
 
 }
