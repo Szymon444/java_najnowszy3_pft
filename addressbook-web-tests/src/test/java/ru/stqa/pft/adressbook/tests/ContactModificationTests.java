99af4b1a4b4a1dfe5dc86f3b1606ad2ccec4cb2b
Szymon444 <59486720+Szymon444@users.noreply.github.com>
2020-06-27 08:39:12 +0200

    1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.pft.adressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -25,6 +26,10 @@ public class ContactModificationTests extends TestBase {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add()
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
